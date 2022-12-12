6c6b8fdb1d1e9516af5b1be0258ac2140e3bc376
Max Base <maxbasecode@gmail.com>
2022-12-12 13:54:16 +0100

    create go file

---
+++ BinaryTree.go
@@ -0,0 +1,12 @@
+/**
+ *
+ * @file BinaryTree.go
+ * @author Max Base (maxbasecode@gmail.com)
+ * @brief Binary Tree Implementation in gO
+ * @version 0.1
+ * @date 2022-12-12
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+package main
