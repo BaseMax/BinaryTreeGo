5987a78608078e7dc065831d3f21b86f5ada8346
Max Base <maxbasecode@gmail.com>
2022-12-12 14:08:19 +0100

    add more examples

--- BinaryTree.go
+++ BinaryTree.go
@@ -160,6 +160,30 @@ func (node *Node) FindLast() *Node {
 	}
 }
 
+// Find the height of the binary tree
+func (tree *GeneralTree) Height() int {
+	if tree.Root == nil {
+		return 0
+	} else {
+		return tree.Root.Height()
+	}
+}
+
+func (node *Node) Height() int {
+	if node == nil {
+		return 0
+	} else {
+		leftHeight := node.Left.Height()
+		rightHeight := node.Right.Height()
+
+		if leftHeight > rightHeight {
+			return leftHeight + 1
+		} else {
+			return rightHeight + 1
+		}
+	}
+}
+
 // Find the depth of the binary tree
 func (tree *GeneralTree) Depth() int {
 	if tree.Root == nil {
@@ -706,5 +730,83 @@ func (node *Node) ReverseDiagonalOrder() {
 }
 
 func main() {
+	// Create a binary tree
+	tree := GeneralTree{}
+	tree.Root = &Node{Value: 1}
+	tree.Root.Left = &Node{Value: 2}
+	tree.Root.Right = &Node{Value: 3}
+	tree.Root.Left.Left = &Node{Value: 4}
+	tree.Root.Left.Right = &Node{Value: 5}
+	tree.Root.Right.Left = &Node{Value: 6}
+	tree.Root.Right.Right = &Node{Value: 7}
+	tree.Root.Left.Left.Left = &Node{Value: 8}
+	tree.Root.Left.Left.Right = &Node{Value: 9}
+	tree.Root.Left.Right.Left = &Node{Value: 10}
+	tree.Root.Left.Right.Right = &Node{Value: 11}
+	tree.Root.Right.Left.Left = &Node{Value: 12}
+	tree.Root.Right.Left.Right = &Node{Value: 13}
+	tree.Root.Right.Right.Left = &Node{Value: 14}
+	tree.Root.Right.Right.Right = &Node{Value: 15}
+	tree.Root.Left.Left.Left.Left = &Node{Value: 16}
+	tree.Root.Left.Left.Left.Right = &Node{Value: 17}
+	tree.Root.Left.Left.Right.Left = &Node{Value: 18}
 
+	// Print the binary tree in level order
+	fmt.Println("Level Order")
+	tree.LevelOrder()
+
+	// Print the binary tree in reverse-level order
+	fmt.Println("Reverse Level Order")
+	tree.ReverseLevelOrder()
+
+	// Print the binary tree in vertical order
+	fmt.Println("Vertical Order")
+	tree.VerticalOrder()
+
+	// Print the binary tree in reverse-vertical order
+	fmt.Println("Reverse Vertical Order")
+	tree.ReverseVerticalOrder()
+
+	// Print the binary tree in diagonal order
+	fmt.Println("Diagonal Order")
+	tree.DiagonalOrder()
+
+	// Print the binary tree in reverse-diagonal order
+	fmt.Println("Reverse Diagonal Order")
+	tree.ReverseDiagonalOrder()
+
+	// Create another tree with the creator function
+	tree2 := NewGeneralTree()
+	tree2.Root = &Node{Value: 1}
+	tree2.Root.Left = &Node{Value: 2}
+	tree2.Root.Right = &Node{Value: 3}
+	tree2.Root.Left.Left = &Node{Value: 4}
+
+	// Print the binary tree in level order
+	fmt.Println("Level Order")
+	tree2.LevelOrder()
+
+	// Print the depth of the tree
+	fmt.Println("Depth")
+	fmt.Println(tree2.Depth())
+
+	// Print the height of the tree
+	fmt.Println("Height")
+	fmt.Println(tree2.Height())
+
+	// Print the number of nodes in the tree
+	fmt.Println("Size")
+	fmt.Println(tree2.Size())
+
+	// Print the number of leaves in the tree
+	fmt.Println("Leaves")
+	fmt.Println(tree2.Leaves())
+
+	// Print the number of full nodes in the tree
+	fmt.Println("Full Nodes")
+	fmt.Println(tree2.FullNodes())
+
+	// Print the number of half nodes in the tree
+	fmt.Println("Half Nodes")
+	fmt.Println(tree2.HalfNodes())
 }
