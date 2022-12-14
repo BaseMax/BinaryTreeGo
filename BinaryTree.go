5f3fca42efd588e5067997b1398094c864f1535d
Max Base <MaxBaseCode@Gmail.Com>
2022-12-14 10:33:45 +0100

    Update BinaryTree.go

--- BinaryTree.go
+++ BinaryTree.go
@@ -2,7 +2,7 @@
  *
  * @file BinaryTree.go
  * @author Max Base (maxbasecode@gmail.com)
- * @brief Binary Tree Implementation in gO
+ * @brief Binary Tree Implementation in Go
  * @version 0.1
  * @date 2022-12-12
  * @copyright Copyright (c) 2022
